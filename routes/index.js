8cb618034822d3b9f10dddd236b0fe49ba1668b6
jegwlee <jegwlee@gmail.com>
2023-06-21 20:36:05 +0900

    [Add] users 라우터 import

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const postsRouter = require('./posts.js');
 const commentsRouter = require('./comments.js');
+const usersRouter = require('../routes/users.js');
 
 router.use('/posts', postsRouter);
 router.use('/comments', commentsRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
