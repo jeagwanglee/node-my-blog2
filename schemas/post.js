602fd5dbf4615dc4aa7fdd75b06326d0af9d393e
jegwlee <jegwlee@gmail.com>
2023-06-21 16:34:46 +0900

    first commit

---
+++ schemas/post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+});
+postSchema.set('timestamps', true);
+module.exports = mongoose.model('Post', postSchema);
