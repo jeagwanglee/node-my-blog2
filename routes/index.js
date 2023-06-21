ef7c440a2640453c0c64566c133209f1710d7df7
jegwlee <jegwlee@gmail.com>
2023-06-21 21:41:43 +0900

    [Add] auth  라우터 import

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const postsRouter = require('./posts.js');
 const commentsRouter = require('./comments.js');
 const usersRouter = require('../routes/users.js');
+const authRouter = require('./auth.js');
 
 router.use('/posts', postsRouter);
 router.use('/comments', commentsRouter);
 router.use('/users', usersRouter);
+router.use('/auth', authRouter);
 
 module.exports = router;
