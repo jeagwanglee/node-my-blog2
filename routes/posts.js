f7135e04bff4ffe648a4a21d936d1447d05a5790
jegwlee <jegwlee@gmail.com>
2023-06-22 12:21:44 +0900

    [Modify] 게시글 수정 API 수정
    이제 authMiddleware로 사용자 인증을 한다.

--- routes/posts.js
+++ routes/posts.js
@@ -89,26 +89,62 @@ router.get('/:_postId', async (req, res) => {
 });
 
 // 게시글 수정 PUT
-router.put('/:_postId', async (req, res) => {
+/**# 412 body 데이터가 정상적으로 전달되지 않는 경우
+{"errorMessage": "데이터 형식이 올바르지 않습니다."}
+
+# 412 Title의 형식이 비정상적인 경우
+{"errorMessage": "게시글 제목의 형식이 일치하지 않습니다."}
+
+# 412 Content의 형식이 비정상적인 경우
+{"errorMessage": "게시글 내용의 형식이 일치하지 않습니다."}
+
+
+
+# 403 게시글을 수정할 권한이 존재하지 않는 경우
+{"errorMessage": "게시글 수정의 권한이 존재하지 않습니다."}
+
+
+
+# 403 Cookie가 존재하지 않을 경우
+{"errorMessage": "로그인이 필요한 기능입니다."}
+
+# 403 Cookie가 비정상적이거나 만료된 경우
+{"errorMessage": "전달된 쿠키에서 오류가 발생하였습니다."}
+
+
+
+# 401 게시글 수정이 실패한 경우
+{"errorMessage": "게시글이 정상적으로 수정되지 않았습니다.”}
+
+# 400 예외 케이스에서 처리하지 못한 에러
+{"errorMessage": "게시글 수정에 실패하였습니다."} */
+
+router.put('/:_postId', authMiddleware, async (req, res) => {
   try {
     const { _postId } = req.params;
-    const { password, user, title, content } = req.body;
-
+    const { title, content } = req.body;
     const existPost = await Post.find({ _id: _postId });
-    if (existPost.length === 0) {
-      return res.status(404).json({ message: '게시글 조회에 실패하였습니다.' });
-    }
 
-    await Post.updateOne({ _id: _postId }, { $set: { user, title, content } });
-    res.status(200).json({ message: '게시글을 수정하였습니다.' });
+    if (!title || !content) {
+      res.status(412).json({ message: '데이터 형식이 올바르지 않습니다.' });
+    } else if (typeof title !== 'string') {
+      res.status(412).json({ errorMessage: '게시글 제목의 형식이 일치하지 않습니다.' });
+    } else if (typeof content !== 'string') {
+      res.status(412).json({ errorMessage: '게시글 내용의 형식이 일치하지 않습니다.' });
+    } else if (existPost.length === 0) {
+      return res.status(404).json({ message: '게시글 조회에 실패하였습니다.' });
+    } else {
+      try {
+        await Post.updateOne({ _id: _postId }, { $set: { title, content } });
+        res.status(200).json({ message: '게시글을 수정하였습니다.' });
+      } catch (error) {
+        res.status(401).json({ message: '게시글이 정상적으로 수정되지 않았습니다.' });
+      }
+    }
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    return res.status(400).json({ message: '데이터 형식이 올바르지 않습니다.' });
+    return res.status(400).json({ message: '게시글 수정에 실패했습니다.' });
   }
-
-  // if (password !== existPost[0].password) {
-  //   return res.status(400).json({ success: false, errorMessage: '비밀번호가 틀렸습니다.' });
-  // }
 });
 
 // 게시글 삭제 DELETE
